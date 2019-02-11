9de75e20e9d43b7be741a59bae7d1ff28a890490
tops-bhadresh <tops.bhadresh@gmail.com>
2019-02-10 22:59:03 -0800

    upload

--- collectioneViewExample/ViewController.swift
+++ collectioneViewExample/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let arr = [
+        ["Image":"img_1.png","Name":"bhadresh"], ["Image":"img_2.png","Name":"ramesh"], ["Image":"img_3.png","Name":"harsh"], ["Image":"img_4.png","Name":"harsha"]
+    ]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -23,3 +26,36 @@ class ViewController: UIViewController {
 
 }
 
+
+extension ViewController:UICollectionViewDelegate,UICollectionViewDataSource{
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return arr.count
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! infoCell
+        
+        let item = arr[indexPath.item]
+        
+        cell.imgForCell.image = UIImage(named: item["Image"]!)
+        cell.titleForCell.text = item["Name"]!
+        
+        return cell
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+}
+
+class infoCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imgForCell: UIImageView!
+    
+    @IBOutlet weak var titleForCell: UILabel!
+    
+    
+}
